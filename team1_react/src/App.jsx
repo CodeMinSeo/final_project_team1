2332e5cd07be57a5728caef60750f34b7eb02913
CodeMinSeo <rnrmf6076@gmail.com>
2025-04-25 15:05:08 +0900

    tailwind

--- team1_react/src/App.jsx
+++ team1_react/src/App.jsx
@@ -1,13 +1,38 @@
-import './App.css'
+import { FaBeer } from '@react-icons/all-files/fa/FaBeer.js';
+import Example from './components/kms/Example.jsx';
+import Header from './components/layout/Header.jsx';
+import Footer from './components/layout/Footer.jsx';
 
 function App() {
 
   return (
     <>
+      <Header />
+
+      <br/><br/><br/>
+
       <div className={'container mt-5'}>
         <h1>Bootstrap Test</h1>
         <button className={'btn btn-primary'}>Bootstrap</button>
       </div>
+
+      <hr/>
+
+      <div className={'container mt-5'}>
+        <h1>React-Icon Test</h1>
+        <h1><FaBeer /></h1>
+      </div>
+
+      <hr/>
+
+      <div className={'container mt-5'}>
+        <h1>Tailwindcss Test</h1>
+        <p className={'text-5xl font-bold underline'}>tailwind</p>
+      </div>
+
+      <br/><br/><br/>
+
+      <Footer />
     </>
 
   )
---
+++ team1_react/src/components/AxiosTest.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+function AxiosTest() {
+    const sendDataTest = () => {
+        axios.get("http://localhost:8080/test")
+        .then(res => {
+            console.log('axios를 사용한 비동기 통신에 성공')
+            console.log(res.data)
+        })
+        .catch(err => {
+            console.log("비동기 통신 중 오류가 발생했습니다.")
+            console.log(err)
+        })
+    }
+    return (
+        <div>
+            <h3 className={'text-center'}>Axios 를 이용한 비동기 통신</h3>
+            <div className={'row'}>
+                <div className={'col-sm-8 mx-auto'}>
+                    <div className={'mt-3 d-flex justify-content-center'}>
+                        <button type={'button'} className={'btn btn-secondary me-2'} onClick={sendDataTest}>Spring RestAPI 서버 접속 테스트</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AxiosTest
