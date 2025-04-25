c8d8c14a770c82694d8219785babb777a89052c8
CodeMinSeo <rnrmf6076@gmail.com>
2025-04-25 12:46:01 +0900

    서버_리액트_부트스트랩_prettierrc

---
+++ team1_react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className={'container mt-5'}>
+        <h1>Bootstrap Test</h1>
+        <button className={'btn btn-primary'}>Bootstrap</button>
+      </div>
+    </>
+
+  )
+}
+
+export default App
